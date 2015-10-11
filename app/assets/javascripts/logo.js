cd1e95c21c0a95c23d3113400a86aa9d8afa5203
Antoine007 <mr.magny+github@gmail.com>
2015-10-12 00:03:04 +0200

    logo as png, metas, tawk to

---
+++ app/assets/javascripts/logo.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){
+//   $('.logo').hover(
+//     function(){ $('#rightxl').removeClass('rightxl'), $('#topxl').removeClass('topxl'), $('#leftxl').removeClass('leftxl')}
+//   )
+// })
